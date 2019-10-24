7d7ee6e16af36ef9ebb9a8a4551446466c3d1755
Ronald Vo <Ronald.Vo@scorpion.co>
2019-10-23 17:35:38 -0700

    1.31

--- src/app/toast.service.ts
+++ src/app/toast.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { ChromeStorageService } from './chrome-storage.service';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
@@ -8,18 +6,12 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class ToastService {
 
-  // private toastBehaviorSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  // public toastObservable = this.toastBehaviorSubject.asObservable();
-  // private chromeWatcher = this.chromeStorageService.linksObservable.subscribe(result => {
-  //   this.toast('Updated!');
-  // });
-
   constructor(private toastBar: MatSnackBar) { }
 
   show(message?: string): void {
     this.toastBar.open(message, null, {
       duration: 2500,
-      verticalPosition: 'top',
+      verticalPosition: 'bottom',
       horizontalPosition: 'left'
     })
   }
